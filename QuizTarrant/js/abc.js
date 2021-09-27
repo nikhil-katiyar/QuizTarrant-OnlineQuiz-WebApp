a0027194c33f88609d72d185efa7f4560f124e61
nikhil-katiyar <56537415+nikhil-katiyar@users.noreply.github.com>
2021-09-28 00:58:16 +0530

    Delete abc.js

--- QuizTarrant/js/abc.js
+++
@@ -1,9 +0,0 @@
-let sub = [
-  "asdfsdsdffffffffffffffffff",
-  "bfsddddddddddddddddddddddddddddd",
-  "cfsddddddddddddddddddddddddddd",
-  "dsfdddddddddddddddddddddddddd",
-  4,
-  7,
-];
-console.log(sub.length);
